f51344c89cb269574fd445c68d4616b1939b06df
Sara Egli <sara.egli.1994@gmail.com>
2019-03-05 11:14:39 +0100

    Control and objects added. One object changes color when triggered.

---
+++ Assets/ChangeColorAtTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ChangeColorAtTrigger : MonoBehaviour
+{
+    public Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        //rend.material.color = Color.black;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("Trigger triggered.");
+            rend.material.color = Color.white;
+        }
+    }
+}
