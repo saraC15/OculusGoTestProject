f7fcd31ad1a8cc05fe9c588d2d397615db98871b
Sara Egli <sara.egli.1994@gmail.com>
2019-03-06 16:49:41 +0100

    Help from Malcom and Johan N

--- Assets/ChangeColorAtTrigger.cs
+++ Assets/ChangeColorAtTrigger.cs
@@ -4,6 +4,7 @@ public class ChangeColorAtTrigger : MonoBehaviour
 {
     public Renderer rend;
     public Color OriginalColor;
+    public VRTK.VRTK_ControllerEvents events; //kan genom att ha publika variabler dra in script som ligger på samma objekt så att de kan påkallas från detta scriptet
 
     // Use this for initialization
     void Start()
@@ -14,18 +15,27 @@ public class ChangeColorAtTrigger : MonoBehaviour
             OriginalColor = 
             this.gameObject.GetComponent<Renderer>().materials[0].color;
         }
+        events = (VRTK.VRTK_ControllerEvents)FindObjectOfType(typeof(VRTK.VRTK_ControllerEvents));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        if (events.AnyButtonPressed()) //kollar om det finns någon knapp som trycks ned - gör istället en event listener för det
         {
-            rend.material.color = Color.white;
+            rend.material.color = Color.white; //Ska trigga VRTK-scriptet
         }
         else
         {
             rend.material.color = OriginalColor;
         }
+        //if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        //{
+        //    rend.material.color = Color.white;
+        //}
+        //else
+        //{
+        //    rend.material.color = OriginalColor;
+        //}
     }
 }
